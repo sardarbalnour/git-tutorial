8c32691be72499561c97105be05f7f5ddacfca4d
Sardar Balnour <balnour.sardar@gmail.com>
2024-01-22 12:22:48 +0330

    client-side storage

--- javascript/script.js
+++ javascript/script.js
@@ -1,1 +1,8 @@
-console.log("hiii");
+// client-side storage season 20
+// local storage
+localStorage.setItem("name", "sardar");
+// JSON.stringify , JSON.parse
+const data = { name: "sardar", age: 23 };
+const srtingData = JSON.stringify(data);
+localStorage.setItem("data", srtingData);
+
