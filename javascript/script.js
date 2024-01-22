f546e9bacbbf1f6fb3ed439b698c9220304b1022
Sardar Balnour <balnour.sardar@gmail.com>
2024-01-22 17:19:34 +0330

    just practicing storages

--- javascript/script.js
+++ javascript/script.js
@@ -1,8 +1,27 @@
-// client-side storage season 20
-// local storage
-localStorage.setItem("name", "sardar");
-// JSON.stringify , JSON.parse
-const data = { name: "sardar", age: 23 };
-const srtingData = JSON.stringify(data);
-localStorage.setItem("data", srtingData);
-
+// client-sie storage season 20
+// // local storage
+// localStorage.setItem("name", "sardar");
+// // JSON.stringify , JSON.parse
+// const data = { name: "sardar", age: 23 };
+// const srtingData = JSON.stringify(data);
+// localStorage.setItem("data", srtingData);
+// getting data
+// const data = localStorage.getItem("data");
+// const parsedData = JSON.parse(data);
+// console.log(parsedData);
+// // update data
+// localStorage.setItem("name", "hasan");
+// console.log(localStorage.length);
+// console.log(localStorage.key(0));
+// session storage
+// sessionStorage.setItem("name", "sardar");
+// const data = { name: "sardar", age: 23 };
+// const srtingData = JSON.stringify(data);
+// sessionStorage.setItem("data", srtingData);
+// const data = sessionStorage.getItem("data");
+// const parsedData = JSON.parse(data);
+// console.log(parsedData);
+// sessionStorage.setItem("name", "hasan");
+// console.log(sessionStorage.length);
+// console.log(sessionStorage.key(0));
+// sessionStorage.removeItem("name")
