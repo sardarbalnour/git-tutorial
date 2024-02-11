db01448cfcc9d747b124d1ddd8b48665926d2172
Sardar Balnour <balnour.sardar@gmail.com>
2024-02-11 17:37:09 +0330

    learning asynchrono in javascript

--- javascript/app.js
+++
@@ -1,9 +0,0 @@
-// import show, { name as firstName, age } from "./script.js";
-import * as all from "./script.js";
-console.log(all);
-// const name = "ali";
-// console.log(name, age, firstName);
-// show();
-// introduce()
-// import introduce from "./script.js"
-// console.log(nameqq);
--- javascript/script.js
+++ javascript/script.js
@@ -1,6 +1,1 @@
-// module in js
-const name = "sardar";
-let age = 23;
-const show = ()=> console.log("hello world");
-// export default show;
-export{name, age, show as default}
+// Javascript is a single-threaded language
