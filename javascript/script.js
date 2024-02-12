1f9fdc941cf9f3d75c08cdf3012e127a32a1c141
Sardar Balnour <balnour.sardar@gmail.com>
2024-02-12 11:24:33 +0330

    callback functions in async js

--- javascript/script.js
+++ javascript/script.js
@@ -1,1 +1,15 @@
 // Javascript is a single-threaded language
+// callback functions in async js
+
+function duplicate(number, callback) {
+  setTimeout(() => {
+    const result = number * 2;
+    callback(result);
+  }, 2000);
+}
+
+const add = (number) => {
+  console.log(number + 5);
+};
+
+duplicate(10, add);
