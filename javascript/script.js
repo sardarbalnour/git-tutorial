f74c18b44597f88e83ac6cd83295a5e462b0f9c5
Sardar Balnour <balnour.sardar@gmail.com>
2024-02-12 11:54:04 +0330

    callback hell

--- javascript/script.js
+++ javascript/script.js
@@ -1,15 +1,23 @@
 // Javascript is a single-threaded language
 // callback functions in async js
+// callback hell
 
-function duplicate(number, callback) {
+function duplicate(number, callback1, callback2) {
   setTimeout(() => {
     const result = number * 2;
-    callback(result);
+    console.log("1", result);
+    callback1(result, callback2);
   }, 2000);
 }
 
-const add = (number) => {
-  console.log(number + 5);
+const add = (number, callback) => {
+  const result = number + 5;
+  console.log("2", result);
+  callback(result);
 };
 
-duplicate(10, add);
+const toMyString = (number) => {
+  console.log("3", `${number}`);
+};
+
+duplicate(10, add, toMyString);
