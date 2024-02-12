869fb0e4768ba171bac2c9385c1aea37999f1bb0
Sardar Balnour <balnour.sardar@gmail.com>
2024-02-12 19:42:06 +0330

    promise

--- javascript/script.js
+++ javascript/script.js
@@ -1,23 +1,10 @@
 // Javascript is a single-threaded language
-// callback functions in async js
-// callback hell
-
-function duplicate(number, callback1, callback2) {
+// promise
+const promise = new Promise((resolve, reject) => {
   setTimeout(() => {
-    const result = number * 2;
-    console.log("1", result);
-    callback1(result, callback2);
-  }, 2000);
-}
+    console.log("1");
+    resolve();
+  }, 1000);
+});
 
-const add = (number, callback) => {
-  const result = number + 5;
-  console.log("2", result);
-  callback(result);
-};
-
-const toMyString = (number) => {
-  console.log("3", `${number}`);
-};
-
-duplicate(10, add, toMyString);
+promise.then(() => console.log("2"));
