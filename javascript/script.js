ff1290c3591414fd44a736bc9468fe764a8b7c0e
Sardar Balnour <balnour.sardar@gmail.com>
2024-02-14 11:04:52 +0330

    solving callback hell with promise

--- javascript/script.js
+++ javascript/script.js
@@ -1,20 +1,27 @@
 // Javascript is a single-threaded language
 // promise
-const random = new Promise((resolve, reject) => {
-  console.log("start");
-  setTimeout(() => {
-    const randomNumber = Math.random();
-    if (randomNumber > 0.5) {
-      resolve(randomNumber);
-    } else {
-      reject("An error occured");
-    }
-  }, 1000);
-});
+const dublicate = (number) => {
+  const promise = new Promise((resolve, reject) => {
+    console.log("start");
+    setTimeout(() => {
+      const result = number * 10;
+      resolve(result);
+    }, 1000);
+  });
+  return promise;
+};
 
-random
-  .then((result) => result * 10)
-  .then((number) => Math.floor(number))
-  .then((num) => console.log(num))
-  .catch((error) => console.log(error));
-// random.catch((error) => console.log(error));
+const add = (number) => {
+  console.log("start add");
+  const promise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const result = number + 5;
+      resolve(result);
+    }, 1000);
+  });
+  return promise;
+};
+
+dublicate(5)
+  .then((number) => add(number))
+  .then((result) => console.log(result));
