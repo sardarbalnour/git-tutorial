a52a6671fced377961e91f763aad4fa307706fb8
Sardar Balnour <balnour.sardar@gmail.com>
2024-02-16 12:36:28 +0330

    XMLHttpRequest

--- javascript/script.js
+++ javascript/script.js
@@ -4,3 +4,11 @@
 // what is AJAX
 // what is JSON
 // REST API
+// XMLHttpRequest
+const xhr = new XMLHttpRequest();
+xhr.open("GET", "https://jsonplaceholder.typicode.com/posts");
+xhr.onload = () => {
+  const { response } = xhr;
+  console.log(JSON.parse(response));
+};
+xhr.send();
