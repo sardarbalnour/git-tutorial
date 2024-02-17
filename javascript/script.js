908064aa5a776b019dd43e91b7fbf2a2eeca8b66
Sardar Balnour <balnour.sardar@gmail.com>
2024-02-17 12:27:45 +0330

    GET and POST methods

--- javascript/script.js
+++ javascript/script.js
@@ -14,12 +14,6 @@
 // };
 // xhr.send();
 
-// fetch
-// fetch("https://jsonplaceholder.typicode.com/posts")
-//   .then((res) => res.json())
-//   .then((data) => console.log(data))
-//   .catch((err) => console.log(err));
-
 // async function getData() {
 //   const res = await fetch("https://jsonplaceholder.typicode.com/posts");
 //   const data = await res.json();
@@ -27,20 +21,40 @@
 // }
 // getData();
 
-function fetchData(url) {
-  const promise = new Promise((resolve, reject) => {
-    const xhr = new XMLHttpRequest();
-    xhr.open("GET", url);
-    xhr.onload = () => {
-      const { response } = xhr;
-      resolve(JSON.parse(response));
-    };
-    xhr.send();
-  });
+// function fetchData(url) {
+//   const promise = new Promise((resolve, reject) => {
+//     const xhr = new XMLHttpRequest();
+//     xhr.open("GET", url);
+//     xhr.onload = () => {
+//       const { response } = xhr;
+//       resolve(JSON.parse(response));
+//     };
+//     xhr.send();
+//   });
+//   return promise;
+// }
+// fetchData("https://jsonplaceholder.typicode.com/posts").then((data) =>
+//   console.log(data)
+// );
 
-  return promise;
-}
+// fetch
+// fetch("https://jsonplaceholder.typicode.com/posts")
+//   .then((res) => res.json())
+//   .then((data) => console.log(data))
+//   .catch((err) => console.log(err));
+const url = "https://jsonplaceholder.typicode.com/posts";
+const data = {
+  title: "hrchi pm xosha",
+  body: "disanish hrchi dlt dyle",
+  userId: "2000",
+};
 
-fetchData("https://jsonplaceholder.typicode.com/posts").then((data) =>
-  console.log(data)
-);
+fetch(url, {
+  method: "POST",
+  body: JSON.stringify(data),
+  headers: {
+    "Content-Type": "application/json",
+  },
+})
+  .then((res) => res.json())
+  .then((data) => console.log(data));
