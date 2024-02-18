3da7d15b879284d7fb1306ca5b2a97d7646aef4d
Sardar Balnour <balnour.sardar@gmail.com>
2024-02-18 11:59:34 +0330

    PUT

--- javascript/script.js
+++ javascript/script.js
@@ -42,19 +42,35 @@
 //   .then((res) => res.json())
 //   .then((data) => console.log(data))
 //   .catch((err) => console.log(err));
-const url = "https://jsonplaceholder.typicode.com/posts";
-const data = {
-  title: "hrchi pm xosha",
-  body: "disanish hrchi dlt dyle",
-  userId: "2000",
-};
+// const url = "https://jsonplaceholder.typicode.com/posts";
+// const data = {
+//   title: "hrchi pm xosha",
+//   body: "disanish hrchi dlt dyle",
+//   userId: "2000",
+// };
 
-fetch(url, {
-  method: "POST",
-  body: JSON.stringify(data),
+// fetch(url, {
+//   method: "POST",
+//   body: JSON.stringify(data),
+//   headers: {
+//     "Content-Type": "application/json",
+//   },
+// })
+//   .then((res) => res.json())
+//   .then((data) => console.log(data));
+
+// PUT
+fetch("https://jsonplaceholder.typicode.com/posts/1", {
+  method: "PUT",
+  body: JSON.stringify({
+    id: 1,
+    title: "shteki tazam nosiva",
+    body: "awash ba dli xom kolan dagorm va update dkmawa",
+    userId: 1,
+  }),
   headers: {
-    "Content-Type": "application/json",
+    "Content-type": "application/json; charset=UTF-8",
   },
 })
-  .then((res) => res.json())
-  .then((data) => console.log(data));
+  .then((response) => response.json())
+  .then((json) => console.log(json));
