1e69e6876f1173bc99a4abd71e3420fea900030b
Sardar Balnour <balnour.sardar@gmail.com>
2024-02-18 12:26:57 +0330

    DELETE

--- javascript/script.js
+++ javascript/script.js
@@ -76,14 +76,21 @@
 //   .then((json) => console.log(json));
 
 // PATCH
+// fetch("https://jsonplaceholder.typicode.com/posts/1", {
+//   method: "PATCH",
+//   body: JSON.stringify({
+//     title: "faqat aw title awaz dkm karm bawani dika ne",
+//   }),
+//   headers: {
+//     "Content-type": "application/json; charset=UTF-8",
+//   },
+// })
+//   .then((response) => response.json())
+//   .then((json) => console.log(json));
+
+// DELETE
 fetch("https://jsonplaceholder.typicode.com/posts/1", {
-  method: "PATCH",
-  body: JSON.stringify({
-    title: "faqat aw title awaz dkm karm bawani dika ne",
-  }),
-  headers: {
-    "Content-type": "application/json; charset=UTF-8",
-  },
+  method: "DELETE",
 })
   .then((response) => response.json())
   .then((json) => console.log(json));
