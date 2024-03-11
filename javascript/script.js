a0c0602e12651988e925089b8b6e05a72d94f539
Sardar Balnour <balnour.sardar@gmail.com>
2024-03-11 17:21:42 +0330

    constructor functions

--- javascript/script.js
+++ javascript/script.js
@@ -1,2 +1,19 @@
 // practicing projects
 // oop learning
+// constructor functions
+function Quiz(question, answers, correctAnswer) {
+  this.question = question;
+  this.answers = answers;
+  this.correctAnswer = correctAnswer;
+  this.showQuestion = function () {
+    console.log(this.question);
+  };
+  this.checkAnswer = function () {
+    console.log("The answer is : ", this.answers[this.correctAnswer]);
+  };
+}
+
+const quiz1 = new Quiz("question text1", [1, 2, 3, 4], 0);
+const quiz2 = new Quiz("question text2", [1, 2, 3, 4], 1);
+console.log(quiz1);
+console.log(quiz2);
