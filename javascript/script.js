261814492678e95308a8335fe76d6c3fb34881eb
Sardar Balnour <balnour.sardar@gmail.com>
2024-03-15 13:38:28 +0330

    prototype

--- javascript/script.js
+++ javascript/script.js
@@ -1,17 +1,46 @@
 // practicing projects
 // oop learning
 // constructor functions
+// function Quiz(question, answers, correctAnswer) {
+//   this.question = question;
+//   this.answers = answers;
+//   this.correctAnswer = correctAnswer;
+//   this.showQuestion = function () {
+//     console.log(this.question);
+//   };
+//   this.checkAnswer = function () {
+//     console.log("The answer is : ", this.answers[this.correctAnswer]);
+//   };
+// }
+
+// const quiz1 = new Quiz("question text1", [1, 2, 3, 4], 0);
+// const quiz2 = new Quiz("question text2", [1, 2, 3, 4], 1);
+// console.log(quiz1);
+// console.log(quiz2);
+
+// prototype
+
 function Quiz(question, answers, correctAnswer) {
   this.question = question;
   this.answers = answers;
-  this.correctAnswer = correctAnswer;
-  this.showQuestion = function () {
-    console.log(this.question);
-  };
-  this.checkAnswer = function () {
-    console.log("The answer is : ", this.answers[this.correctAnswer]);
-  };
+  // this.correctAnswer = correctAnswer;
+  // this.showQuestion = function () {
+  //   console.log(this.question);
+  // };
+  // this.checkAnswer = function () {
+  //   console.log("The answer is : ", this.answers[this.correctAnswer]);
+  // };
 }
+
+Quiz.prototype.showQuestion = function () {
+  console.log(this.question);
+};
+
+Quiz.prototype.checkAnswer = function () {
+  console.log("The answer is : ", this.answers[this.correctAnswer]);
+};
+
+Quiz.prototype.constant = 5;
 
 const quiz1 = new Quiz("question text1", [1, 2, 3, 4], 0);
 const quiz2 = new Quiz("question text2", [1, 2, 3, 4], 1);
