da14765d41ed5d6a8e4e5ea72c69b3dd38da33d7
Sardar Balnour <balnour.sardar@gmail.com>
2024-03-16 11:55:27 +0330

    class declaration

--- javascript/script.js
+++ javascript/script.js
@@ -20,29 +20,60 @@
 
 // prototype
 
-function Quiz(question, answers, correctAnswer) {
-  this.question = question;
-  this.answers = answers;
-  // this.correctAnswer = correctAnswer;
-  // this.showQuestion = function () {
-  //   console.log(this.question);
-  // };
-  // this.checkAnswer = function () {
-  //   console.log("The answer is : ", this.answers[this.correctAnswer]);
-  // };
-}
+// function Quiz(question, answers, correctAnswer) {
+//   this.question = question;
+//   this.answers = answers;
+//   // this.correctAnswer = correctAnswer;
+//   // this.showQuestion = function () {
+//   //   console.log(this.question);
+//   // };
+//   // this.checkAnswer = function () {
+//   //   console.log("The answer is : ", this.answers[this.correctAnswer]);
+//   // };
+// }
 
-Quiz.prototype.showQuestion = function () {
-  console.log(this.question);
-};
+// Quiz.prototype.showQuestion = function () {
+//   console.log(this.question);
+// };
 
-Quiz.prototype.checkAnswer = function () {
-  console.log("The answer is : ", this.answers[this.correctAnswer]);
-};
+// Quiz.prototype.checkAnswer = function () {
+//   console.log("The answer is : ", this.answers[this.correctAnswer]);
+// };
 
-Quiz.prototype.constant = 5;
+// Quiz.prototype.constant = 5;
+
+// const quiz1 = new Quiz("question text1", [1, 2, 3, 4], 0);
+// const quiz2 = new Quiz("question text2", [1, 2, 3, 4], 1);
+// console.log(quiz1);
+// console.log(quiz2);
+
+// classes
+// classes are a template for creating objects
+
+class Quiz {
+  constructor(question, answers, correctAnswer) {
+    this.question = question;
+    this.answers = answers;
+    this.correctAnswer = correctAnswer;
+    // this.showQuestion = function () {
+    //   console.log(this.question);
+    // };
+    // this.checkAnswer = function () {
+    //   console.log("The answer is : ", this.answers[this.correctAnswer]);
+    // };
+    // age ye metode xas dashte bashim inja minivisimesh
+    // vagar na age tekrari bashe vase hame payin minivisim ke ersbari beshe
+  }
+
+  // prototype
+  showQuestion() {
+    console.log(this.question);
+  }
+
+  checkAnswer() {
+    console.log("The answer is : ", this.answers[this.correctAnswer]);
+  }
+ }
 
 const quiz1 = new Quiz("question text1", [1, 2, 3, 4], 0);
-const quiz2 = new Quiz("question text2", [1, 2, 3, 4], 1);
 console.log(quiz1);
-console.log(quiz2);
