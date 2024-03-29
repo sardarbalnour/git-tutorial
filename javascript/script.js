850fe7a1953499136456087cf3d0d40ae6c71abe
Sardar Balnour <balnour.sardar@gmail.com>
2024-03-29 17:58:32 +0330

    What is REGEX

---
+++ javascript/script.js
@@ -0,0 +1,5 @@
+// regex : regular expression
+// is a way to describe a pattern
+// it is used to locate or validate specific strings or
+// patterns of text in a sentence, document, or any other
+// character input
