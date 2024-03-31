5f642869f7408e5d686e51d5fc3d59488107a7e3
Sardar Balnour <balnour.sardar@gmail.com>
2024-03-31 11:29:30 +0330

    replace method in regex

--- javascript/script.js
+++ javascript/script.js
@@ -5,7 +5,12 @@
 // character input
 // learning regex
 
-const email = "balnour.sardar@gmail.com";
-const regex = /^[\w_\.]+@[a-zA-Z]+\.[a-zA-Z]{2,3}$/g;
-const result = regex.test(email);
-console.log(result);
+// const email = "balnour.sardar@gmail.com";
+// const regex = /^[\w_\.]+@[a-zA-Z]+\.[a-zA-Z]{2,3}$/g;
+// const result = regex.test(email);
+// console.log(result);
+
+const str = "Hi; I am Sardar and my phone number is 09055125636";
+const regex = /09\d{9}/;
+const newStr = str.replace(regex, "*********");
+console.log(newStr);
