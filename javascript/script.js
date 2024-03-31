a17c68d606e7492280d4f95aa3296c7d374a3bb8
Sardar Balnour <balnour.sardar@gmail.com>
2024-03-31 11:18:20 +0330

    regex test method

--- javascript/script.js
+++ javascript/script.js
@@ -4,3 +4,8 @@
 // patterns of text in a sentence, document, or any other
 // character input
 // learning regex
+
+const email = "balnour.sardar@gmail.com";
+const regex = /^[\w_\.]+@[a-zA-Z]+\.[a-zA-Z]{2,3}$/g;
+const result = regex.test(email);
+console.log(result);
