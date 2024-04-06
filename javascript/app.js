56b78609d637af96ebd147b786bb3185633918b3
Sardar Balnour <balnour.sardar@gmail.com>
2024-04-06 16:17:11 +0330

    axios library

--- javascript/app.js
+++ javascript/app.js
@@ -1,1 +1,27 @@
-console.log("hello world");
+// Third-Party libraries
+
+// axios
+//   .get("https://jsonplaceholder.typicode.com/todos")
+//   .then((response) => console.log(response));
+
+// axios
+//   .get("/https://jsonplaceholder.typicode.com/todos")
+//   .then(function (response) {
+//     // handle success
+//     console.log(response);
+//   })
+//   .catch(function (error) {
+//     // handle error
+//     console.log(error);
+//   })
+//   .finally(function () {
+//     // always executed
+//   });
+
+const getReq = async () => {
+  const response = await axios.get(
+    "https://jsonplaceholder.typicode.com/todos"
+  );
+  console.log(response);
+};
+getReq();
